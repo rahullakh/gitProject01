aac5e88c19827902965fdd6e5500c866a155b26a
Your Name <dharmadudu@gmail.com>
2025-03-04 18:44:34 +0530

    Chanes now

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ class App{
     public static void main (String[] args){
         System.out.println("Hello World!");
     }
-    public void m1(){
+    public void payment(){
         System.out.println("m1");
 
     }
